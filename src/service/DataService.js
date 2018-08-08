604ea968a5c12f09318fb7ceb714cfda9fe4ff23
Trilok Chand <trilokc@Virtusa.com>
2018-08-08 15:35:18 +0530

    react-cam added

--- src/service/DataService.js
+++ src/service/DataService.js
@@ -9,13 +9,13 @@ export default class DataService extends React.Component {
             data: [],
             pageNumber: 1
         };
-        
+
     }
     componentDidMount() {
         this.dataHandler();
     }
-    dataHandler(event) {   
-        console.log("in page number:", this.state.pageNumber);     
+    dataHandler(event) {
+        console.log("in page number:", this.state.pageNumber);
         fetch("https://reqres.in/api/users?page=" + this.state.pageNumber)
             .then(result => {
                 return result.json();
@@ -24,25 +24,25 @@ export default class DataService extends React.Component {
                 this.setState({
                     data: json.data
                 })
-                console.log('service data:', this.state.data);                
+                console.log('service data:', this.state.data);
             })
     }
 
-    nextPageHandler(event) {        
+    nextPageHandler(event) {
         this.setState({
             pageNumber: this.state.pageNumber + 1
-        }, this.dataHandler);                   
+        }, this.dataHandler);
     }
-    previousPageHandler(event) {       
+    previousPageHandler(event) {
         this.setState({
             pageNumber: this.state.pageNumber - 1
-        }, this.dataHandler);        
-        
+        }, this.dataHandler);
+
     }
 
     rows = () => {
         this.state.data.map(row => {
-            return (<tr className={row.id%2==0 ? 'odd' : 'even'}>
+            return (<tr className={row.id % 2 == 0 ? 'odd' : 'even'}>
                 <td>{row.id}</td>
                 <td>{row.first_name}</td>
                 <td>{row.last_name}</td>
@@ -50,18 +50,23 @@ export default class DataService extends React.Component {
             )
         });
     }
+    onSort(sortKey, e) {
+        const data = this.state.data;
+        data.sort((a, b) => a[sortKey].localeCompare(b[sortKey]))
+        this.setState({ data })
+    }
     render() {
         return (
-            <div>
+            <div style={{border: "solid 2px", width: "300px", align: "center"}}>
                 {/* <button className="btn btn-success" onClick={this.dataHandler.bind(this)}> Click to get data</button> */}
-                <button style={{marginRight: "10px"}} 
-                    className="btn btn-success" 
-                    onClick={this.previousPageHandler.bind(this)} 
-                    disabled={this.state.pageNumber <= 1}> previous page 
+                <button style={{ marginRight: "10px" }}
+                    className="btn btn-success"
+                    onClick={this.previousPageHandler.bind(this)}
+                    disabled={this.state.pageNumber <= 1}> previous page
                 </button>
-                <button className="btn btn-success" 
-                    onClick={this.nextPageHandler.bind(this)} 
-                    disabled={this.state.pageNumber >= 4}> next page 
+                <button className="btn btn-success"
+                    onClick={this.nextPageHandler.bind(this)}
+                    disabled={this.state.pageNumber >= 4}> next page
                 </button>
                 <p>Page number : {this.state.pageNumber}</p>
                 <table align="center">
@@ -72,11 +77,11 @@ export default class DataService extends React.Component {
                     </thead>
                     <tr>
                         <td></td>
-                        <td></td>
-                        <td></td>
+                        <td onClick={this.onSort.bind(this, 'first_name')}>sort</td>
+                        <td onClick={this.onSort.bind(this, 'last_name')}>sort</td>
                     </tr>
                     {this.state.data.map((row, index) => {
-                        return (<tr className={index%2==0 ? 'even' : 'odd'}>
+                        return (<tr className={index % 2 == 0 ? 'even' : 'odd'}>
                             <td>{row.id}</td>
                             <td>{row.first_name}</td>
                             <td>{row.last_name}</td>
